05c419009f765e386e8de06758aa0dc64307aded
Fejluiza <fejluiza@gmail.com>
2024-05-24 11:44:45 -0300

    Lista de contatos

---
+++ src/components/listaContatos.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import './listaContatos.css';
+
+function ListaDeContatos (){
+    const [contatos, setContatos] = useState ([])
+    const [nome, setNome] = useState('')
+    const [telefone, setTelefone] = useState('')
+
+    const adicionarContato = () => {
+        if (nome&&telefone){
+            setContatos([...contatos,{nome, telefone}])
+            setNome('')
+            setTelefone('')
+        }
+    }
+
+    return (
+        <>
+        <h2>Lista de Contatos</h2>
+        <div>
+            <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Nome"/>
+            <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} placeholder="Telefone"/>
+            <button onClick={adicionarContato}>Adicionar Contato</button>
+        </div>
+        <ul>{contatos.map((contato, index) =>(
+            <li key={index}><strong>{contato.nome}:</strong>{contato.telefone}</li>
+        ))}
+        </ul>
+        </>
+    )
+}
+
+export default ListaDeContatos
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import ListaDeContatos from './components/listaContatos'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <ListaDeContatos />
+  </React.StrictMode>
+)
+r
